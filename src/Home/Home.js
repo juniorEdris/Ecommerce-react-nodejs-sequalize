8004e3b3fd493eabe5b2335ca328bcbd61b9933c
Eftekar raghib <mdraghib17@gmail.com>
2021-03-09 15:32:09 +0600

    card changed

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,30 +1,26 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { SectionHeaderOne } from '../PrimaryComponents/SectionHeader/SectionHeaderOne'
-import DecoreBanner from './Components/DecoreBanner'
-import DecoreProducts from './Components/DecoreProducts'
-import  ProductSecion  from './Components/PrductSecion'
-import { WideBanner } from './Components/WideBanner'
-import './homepage.css'
+import React from 'react';
+import { connect } from 'react-redux';
+import { SectionHeaderOne } from '../PrimaryComponents/SectionHeader/SectionHeaderOne';
+import DecoreBanner from './Components/DecoreBanner';
+import DecoreProducts from './Components/DecoreProducts';
+import ProductSecion from './Components/PrductSecion';
+import { WideBanner } from './Components/WideBanner';
+import './homepage.css';
 
 export const Home = (props) => {
-    return (
-        <div className='home_page col-12'>
-            <WideBanner />
-            <SectionHeaderOne fontStyleOne={'Best'} fontStyleTwo={ 'Selling'}/>
-            <ProductSecion />
-            <DecoreBanner/>
-            <DecoreProducts/>
-        </div>
-    )
-}
+  return (
+    <div className="home_page col-12">
+      <WideBanner />
+      <SectionHeaderOne fontStyleOne={'Best'} fontStyleTwo={'Selling'} />
+      <ProductSecion />
+      <DecoreBanner />
+      <DecoreProducts />
+    </div>
+  );
+};
 
-const mapStateToProps = (state) => ({
-    
-})
+const mapStateToProps = (state) => ({});
 
-const mapDispatchToProps = {
-    
-}
+const mapDispatchToProps = {};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home)
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/PrimaryComponents/Product/Product.js
+++ src/PrimaryComponents/Product/Product.js
@@ -1,33 +1,81 @@
-import React from 'react'
-import './product.css'
+import React from 'react';
+import './product.css';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
+export const Product = (props) => {
+  const count = 1;
+  return (
+    <div className="product-section col col-md-3">
+      <div className={`product-item `} id={props.product.id}>
+        <div className="product-thumb">
+          <Link to={`/productdetails?id=${props.product.id}`}>
+            <img
+              src={`assets/images/homepage/${props.product.image}`}
+              className="pri-img"
+              alt={props.product.name}
+            />
+          </Link>
 
-function Product({ id, title, price, rating, image }) {
-
-    return (
-        <div className='product col col-sm-4' key={ id}>
-                <div className="">
-                    <img className='product__image' src={image} alt='' />
-                </div>
-                <div className='product__info'>
-                    <h4>{title}</h4>
-                    <p className='product__price'>
-                        <small>$</small>
-                        <strong>{price}</strong>
-                    </p>
-                    <div className='product__ratings'>
-                        {
-                            Array(rating)
-                            .fill()
-                            .map(_ => (
-                                <p> &#9733; </p>
-                                ))
-                        }
-                    </div>
-                </div>
-                <button  className="product__button btn">Add to basket</button>
+          <div className="box-label">
+            <div className="label-product label_sale">
+              <span>{props.product.sale ? `-${props.product.sale}%` : ''}</span>
             </div>
-    )
-}
+          </div>
+        </div>
+        <div className="product-caption">
+          <div className="product-name">
+            <h4>
+              <Link
+                to={`/productdetails?id=${props.product.id}`}
+                title={props.product.name}>
+                {props.product.name}
+              </Link>
+            </h4>
+          </div>
+          <div className="manufacture-product">
+            <span>
+              <Link to={`/`}>by {props.product.brand}</Link>
+            </span>
+          </div>
+          <div className="price-box">
+            <span className="regular-price">
+              <span className={` ${props.product.special && 'special-price'}`}>
+                £{props.product.price}
+              </span>
+            </span>
+            <span className="old-price">
+              <span>MRP:</span>
+              <del>
+                {props.product.previous_price
+                  ? `£${props.product.previous_price}`
+                  : ''}
+              </del>
+            </span>
+          </div>
+          {count > 0 ? (
+            <div className="single_product_qty mr-3">
+              <span class="dec qtybtn">
+                <i class="fa fa-minus"></i>
+              </span>
+              <input type="text" defaultValue={0} className="" />
+              <span class="inc qtybtn">
+                <i class="fa fa-plus"></i>
+              </span>
+            </div>
+          ) : (
+            <button className="btn-cart" type="button">
+              add to cart
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Product
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Product);
